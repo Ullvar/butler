2c457b2d047d8784f5a6d772798d3bf288cfa8c6
ullvar <ullvarb@gmail.com>
2024-01-10 07:11:47 +0100

    .

---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
+	"google.golang.org/api/option"
+)
+
+func getClient(config *oauth2.Config) *http.Client {
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.Background(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func main() {
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, gmail.MailGoogleComScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	ctx := context.Background()
+	srv, err := gmail.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Gmail client: %v", err)
+	}
+
+	user := "me"
+	r, err := srv.Users.Messages.List(user).MaxResults(2).Do() // Limiting to the latest 10 messages for example
+	if err != nil {
+		log.Fatalf("Unable to retrieve messages: %v", err)
+	}
+
+	if len(r.Messages) == 0 {
+		fmt.Println("No messages found.")
+	} else {
+		fmt.Println("Messages:")
+		for _, m := range r.Messages {
+			msg, err := srv.Users.Messages.Get(user, m.Id).Format("full").Do()
+			if err != nil {
+				log.Printf("Unable to retrieve message %v: %v", m.Id, err)
+				continue
+			}
+			labels, err := srv.Users.Labels.List(user).Do()
+			for _, label := range labels.Labels {
+				fmt.Println(label.Name)
+			}
+			fmt.Println(msg.LabelIds)
+			for _, header := range msg.Payload.Headers {
+				fmt.Println(header)
+				if header.Name == "Subject" {
+					fmt.Printf("- %s\n", header.Value)
+					break
+				}
+			}
+		}
+	}
+}
