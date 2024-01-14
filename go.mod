e146d961dcfe862581e028961e3fadf4bec41282
ullvar <ullvarb@gmail.com>
2024-01-14 14:04:57 +0100

    Updates

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.21.5
 require (
 	cloud.google.com/go/compute v1.23.3 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/bubbletea v0.25.0 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
@@ -14,6 +17,15 @@ require (
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.46.1 // indirect
 	go.opentelemetry.io/otel v1.21.0 // indirect
@@ -22,9 +34,11 @@ require (
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/oauth2 v0.16.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/api v0.155.0 // indirect
+	google.golang.org/api v0.156.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240108191215-35c7eff3a6b1 // indirect
 	google.golang.org/grpc v1.60.1 // indirect
--- main.go
+++ main.go
@@ -3,16 +3,40 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"sort"
+	"strings"
+	"time"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/gmail/v1"
 	"google.golang.org/api/option"
 )
+
+type Message struct {
+	Id      string
+	Labels  []string
+	Subject string
+	Sender  string
+}
+
+type Label struct {
+	Id   string
+	Name string
+}
+
+type Event struct {
+	Summary     string
+	StartDate   string
+	StartTime   time.Time
+	EndDateTime string
+}
 
 func getClient(config *oauth2.Config) *http.Client {
 	tokFile := "token.json"
@@ -61,14 +85,9 @@ func saveToken(path string, token *oauth2.Token) {
 	json.NewEncoder(f).Encode(token)
 }
 
-func main() {
-	b, err := os.ReadFile("credentials.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
-
+func read_mail(b []byte, numberOfMessages *int64, labelsToSearch *string) {
 	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, gmail.MailGoogleComScope)
+	config, err := google.ConfigFromJSON(b, gmail.MailGoogleComScope, calendar.CalendarReadonlyScope)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
@@ -80,34 +99,176 @@ func main() {
 		log.Fatalf("Unable to retrieve Gmail client: %v", err)
 	}
 
+	labels := []Label{}
+	resp, err := srv.Users.Labels.List("me").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve labels: %v", err)
+	}
+	for _, l := range resp.Labels {
+		labels = append(labels, Label{Id: l.Id, Name: l.Name})
+	}
+
 	user := "me"
-	r, err := srv.Users.Messages.List(user).MaxResults(2).Do() // Limiting to the latest 10 messages for example
+	convertedLabelsToSearch := []string{}
+	for _, label := range strings.Split(*labelsToSearch, ",") {
+		for _, l := range labels {
+			if l.Name == label {
+				convertedLabelsToSearch = append(convertedLabelsToSearch, l.Id)
+				break
+			}
+		}
+	}
+	r, err := srv.Users.Messages.List(user).LabelIds(convertedLabelsToSearch...).MaxResults(*numberOfMessages).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve messages: %v", err)
 	}
 
+	messages := []Message{}
+
 	if len(r.Messages) == 0 {
 		fmt.Println("No messages found.")
 	} else {
-		fmt.Println("Messages:")
 		for _, m := range r.Messages {
 			msg, err := srv.Users.Messages.Get(user, m.Id).Format("full").Do()
 			if err != nil {
 				log.Printf("Unable to retrieve message %v: %v", m.Id, err)
 				continue
 			}
-			labels, err := srv.Users.Labels.List(user).Do()
-			for _, label := range labels.Labels {
-				fmt.Println(label.Name)
-			}
-			fmt.Println(msg.LabelIds)
+			subject := ""
+			from := ""
 			for _, header := range msg.Payload.Headers {
-				fmt.Println(header)
 				if header.Name == "Subject" {
-					fmt.Printf("- %s\n", header.Value)
+					subject = header.Value
 					break
 				}
+				if header.Name == "Return-Path" {
+					from = strings.ReplaceAll(strings.Split(header.Value, "@")[1], ">", "")
+				}
+				if header.Name == "From" {
+					from = header.Value
+				}
+			}
+			messages = append(messages, Message{Id: m.Id, Labels: msg.LabelIds, Subject: subject, Sender: from})
+		}
+	}
+
+	fmt.Println("")
+	for _, m := range messages {
+		fmt.Println("\033[1mSubject:", strings.TrimSpace(m.Subject), "\033[0m")
+		fmt.Println("Sender:", m.Sender)
+		fmt.Println("")
+	}
+}
+
+func parseDate(dateStr string) time.Time {
+	t, err := time.Parse(time.RFC3339, dateStr)
+	if err != nil {
+		// If it fails to parse as date-time, try parsing as date-only
+		t, err = time.Parse("2006-01-02", dateStr)
+		if err != nil {
+			fmt.Println("Error parsing date:", err)
+			return time.Time{}
+		}
+	}
+	return t
+}
+
+func sortEvents(events []Event) {
+	sort.Slice(events, func(i, j int) bool {
+		return events[i].StartTime.Before(events[j].StartTime)
+	})
+}
+
+func read_calendar(b []byte) {
+	config, err := google.ConfigFromJSON(b, calendar.CalendarReadonlyScope, gmail.MailGoogleComScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	ctx := context.Background()
+	srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Calendar client: %v", err)
+	}
+
+	calendarId := "primary"
+	from := time.Now().Format(time.RFC3339)
+	yyyy, mm, dd := time.Now().Date()
+	tomorrow := time.Date(yyyy, mm, dd+1, 23, 59, 59, 0, time.Now().Location())
+	calendarEvents, err := srv.Events.List(calendarId).TimeMin(from).TimeMax(tomorrow.Format(time.RFC3339)).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
+	}
+
+	events := []Event{}
+
+	for _, item := range calendarEvents.Items {
+		startDate := ""
+		if item.Start != nil && item.Start.DateTime != "" {
+			startDate = item.Start.DateTime
+		} else if item.Start != nil && item.Start.Date != "" {
+			startDate = item.Start.Date
+		}
+		endDateTime := ""
+		if item.End != nil && item.End.DateTime != "" {
+			endDateTime = item.End.DateTime
+		}
+
+		events = append(events, Event{Summary: item.Summary, StartDate: startDate, StartTime: parseDate(startDate), EndDateTime: endDateTime})
+	}
+
+	sortEvents(events)
+
+	todayName := time.Now().Format("Monday")
+
+	fmt.Println("")
+	for _, event := range events {
+		t, err := time.Parse(time.RFC3339, event.EndDateTime)
+		eventDay := event.StartTime.Local().Format("Monday")
+		if err != nil {
+			if eventDay == todayName {
+				fmt.Println("\033[1m***** ", strings.Replace(event.StartTime.Local().Format("Monday"), todayName, "Today", -1), "all day", " *****\033[0m")
+				fmt.Println("\033[1m", strings.TrimSpace(event.Summary), "\033[0m")
+				fmt.Println("")
+			} else {
+				fmt.Println("***** ", strings.Replace(event.StartTime.Local().Format("Monday"), todayName, "Today", -1), "all day", " *****")
+				fmt.Println(strings.TrimSpace(event.Summary))
+				fmt.Println("")
+			}
+		} else {
+			if eventDay == todayName {
+				fmt.Println("\033[1m***** ", strings.Replace(event.StartTime.Local().Format("Monday 15:04"), todayName, "Today", -1), "-", t.Local().Format("15:04"), " *****\033[0m")
+				fmt.Println("\033[1m", strings.TrimSpace(event.Summary), "\033[0m")
+				fmt.Println("")
+			} else {
+				fmt.Println("***** ", strings.Replace(event.StartTime.Local().Format("Monday 15:04"), todayName, "Today", -1), "-", t.Local().Format("15:04"), " *****")
+				fmt.Println(strings.TrimSpace(event.Summary))
+				fmt.Println("")
 			}
 		}
+	}
+}
+
+func main() {
+	var mail = flag.Bool("mail", false, "show mail")
+	var calendar = flag.Bool("cal", false, "show calendar")
+	var numberOfMessages = flag.Int64("n", 100, "number of messages")
+	var labelsToSearch = flag.String("l", "UNREAD", "labels to search (case sensitive)")
+
+	flag.Parse()
+
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	if *mail {
+		read_mail(b, numberOfMessages, labelsToSearch)
+	} else if *calendar {
+		read_calendar(b)
+	} else {
+		fmt.Println("please specify -mail or -cal")
+
 	}
 }
